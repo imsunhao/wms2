63d66532f16d3f5acfc9898ae1307ec736e7e5c8
imsunhao <sunhao__159@163.com>
2017-07-05 14:42:24 +0800

    猜想 动态配置 查看 权限

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,19 +1,22 @@
 <template>
   <el-row>
-    <el-table :data="doneTodos">
+    <el-checkbox-group
+      v-model="checkedCities1"
+      :min="1"
+      :max="4">
+      <el-checkbox v-for="city in cities" :label="city" :key="city.prop">{{city.label}}</el-checkbox>
+    </el-checkbox-group>
+    <el-table :data="doneTodos" border>
       <el-table-column
+        align="center"
         type="index"
         width="80"></el-table-column>
       <el-table-column
-        label="名称"
+        v-for="value in checkedCities1"
+        :label="value.label"
+        :prop="value.prop"
         show-overflow-tooltip
-        align="center"
-        prop="text"></el-table-column>
-      <el-table-column
-        label="id"
-        show-overflow-tooltip
-        align="center"
-        prop="id"></el-table-column>
+        align="center"></el-table-column>
       <el-table-column
         label="可用操作"
         align="center">
@@ -38,11 +41,23 @@
 <script>
   import { mapState, mapGetters } from 'vuex';
   import Func from '../config/function/0_Home';
+
+  const cityOptions = [
+    {prop: 'id', label: '商品 ID'},
+    {prop: 'name', label: '商品名称'},
+    {prop: 'category', label: '商品分类'},
+    {prop: 'desc', label: '商品描述'},
+    {prop: 'shop', label: '所属店铺'},
+    {prop: 'shopId', label: '店铺 ID'},
+    {prop: 'address ', label: '店铺地址'},
+  ];
   
   export default {
     name: 'home',
     data () {
       return {
+        checkedCities1: [cityOptions[0], cityOptions[1]],
+        cities: cityOptions,
         localCount: 10,
       };
     },
@@ -55,7 +70,6 @@
     },
     methods: {
       a (number) {
-        console.log(this);
         this[Func[number]]();
       },
       [Func[0]] () {
--- src/main.js
+++ src/main.js
@@ -3,11 +3,8 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import router from './router';
-
 import ElementUI from 'element-ui';
-
 import 'element-ui/lib/theme-default/index.css';
-
 import App from './App.vue';
 
 Vue.config.productionTip = true;
@@ -19,14 +16,44 @@ const store = new Vuex.Store({
   state: {
     count: 0,
     todos: [
-      { id: 1, text: 'hfgh', done: true },
-      { id: 2, text: 'baba', done: false },
-      { id: 3, text: 'lala', done: true },
+      {
+        id: '12987122',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987123',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987125',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      }, {
+        id: '12987126',
+        name: '好滋好味鸡蛋仔',
+        category: '江浙小吃、小吃零食',
+        desc: '荷兰优质淡奶，奶香浓而不腻',
+        address: '上海市普陀区真北路',
+        shop: '王小虎夫妻店',
+        shopId: '10333',
+      },
     ],
   },
   getters: {
     doneTodos: state => {
-      return state.todos.filter(todo => todo.done);
+      return state.todos;
     },
   },
   mutations: {
