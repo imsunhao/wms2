301fd048a78d496119bf2c7ed69a449b9c240169
imsunhao <sunhao__159@163.com>
2017-07-11 11:47:16 +0800

    调整 Mutations 方法 现在可以传递参数 进行 VueX 中 值的 修改。 整合 Mutations f，方法

--- src/config/Mutations/index.js
+++ src/config/Mutations/index.js
@@ -1,10 +1,13 @@
 function MutationsMethods (Mutations) {
   let step = {};
-  Mutations.forEach(({ event_name, commit, payload }) => {
+  Mutations.forEach(({event_name, commit, payload}) => {
     step[event_name] = function () {
       this.$store.commit(commit, payload(this));
     };
   });
+  step.f = function (number, data) {
+    this.$store.commit(Mutations[number].commit, Mutations[number].payload(this, data));
+  };
   return step;
 }
-export {MutationsMethods};
+export { MutationsMethods };
