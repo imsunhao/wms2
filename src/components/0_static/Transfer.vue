9eb7c188ee6d101f662dea8e68fbd7842ad76dac
imsunhao <sunhao__159@163.com>
2017-07-19 18:44:01 +0800

    添加 Transfer render的联系

---
+++ src/components/0_static/Transfer.vue
@@ -0,0 +1,50 @@
+<template>
+  <el-transfer
+    :render-content="content"
+    v-model="value1"
+    :data="data"></el-transfer>
+</template>
+
+<script>
+  export default {
+    data () {
+      const generateData = _ => {
+        const data = [];
+        for (let i = 1; i <= 15; i++) {
+          data.push({
+            key: i,
+            label: `备选项 ${i}`,
+            _class: {
+              clicked: false,
+            },
+          });
+        }
+        return data;
+      };
+      return {
+        data: generateData(),
+        value1: [1, 4],
+        content (h, option) {
+          return h('div', {
+            'class': option._class,
+            on: {
+              click () {
+                option._class.clicked = !option._class.clicked;
+                console.log(option._class.clicked);
+              },
+            },
+          }, option.label);
+        },
+      };
+    },
+  };
+</script>
+<style lang="scss">
+.clicked{
+  position: absolute;
+  background: rgba(0,0,0,0.5);
+  width: calc(100% - 48px);
+  left: 0;
+  padding-left: 48px;
+}
+</style>
