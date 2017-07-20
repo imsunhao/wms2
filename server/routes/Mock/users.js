4af7f5d899c4682b3d3c6f1444558921012c4cfd
imsunhao <sunhao__159@163.com>
2017-07-20 15:35:02 +0800

    用户管理 添加 请求过程

--- server/routes/Mock/users.js
+++ server/routes/Mock/users.js
@@ -39,22 +39,19 @@ module.exports = function (router) {
   const consoleOutPut = '\t---->\t'
 
   router.post('/users/Login', function (req, res, next) {
-    let status = false
     let data = req.body
     if (typeof req.session.user !== 'undefined') {
-      req.conso += '用户身份验证'
+      req.conso += consoleOutPut + '用户身份验证\n'
       res.send({status: 3, data: req.session.user})
-      status = true
-      next()
+      return next()
     }
     else if (typeof data.code !== 'undefined' && data.code != 0 && data.code != 1) {
-      req.conso += '用户尝试使用 code 登录...\n'
+      req.conso += consoleOutPut + '用户尝试使用 code 登录...\n'
       for (item of Users) {
         if (item.code === data.code) {
-          req.conso += item.nickname + ' 使用 code 登录！\n'
+          req.conso += consoleOutPut + item.nickname + ' 使用 code 登录！\n'
           req.session.user = item
           res.send({status: 4, data: item})
-          status = true
           return next()
         }
       }
@@ -64,32 +61,35 @@ module.exports = function (router) {
     }
     else {
       if (typeof data.username !== 'undefined') {
-        req.conso += '用户登录'
+        req.conso += consoleOutPut + '用户登录\n'
         for (item of Users) {
           if (item.username === data.username) {
             if (item.password === data.password) {
               req.session.user = item
               res.send({status: 1, data: item})
+              req.conso += consoleOutPut + '用户有效\n'
               return next()
             } else {
               res.send({status: 10000})
+              req.conso += consoleOutPut + '密码无效\n'
               return next()
             }
           }
         }
         res.send({status: 10001})
+        req.conso += consoleOutPut + '无此用户\n'
         return next()
       } else {
         res.send({status: 10003})
-        next()
+        return next()
       }
     }
   })
 
   router.post('/users/logout', function (req, res, next) {
-    req.conso += '用户退出'
+    req.conso += consoleOutPut + '用户退出\n'
     req.session.destroy()
     res.send({status: 2})
-    next()
+    return next()
   })
 }
